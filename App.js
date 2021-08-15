88289d7056e0ab76a0dd3f7e74b7c5c90934415a
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-08-15 16:49:48 +0530

    created basic app

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View,BackHandler,Platform,} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  webView = {
+    canGoBack: false,
+    ref: null,
+  }
+
+  onAndroidBackPress = () => {
+    if (this.webView.canGoBack && this.webView.ref) {
+      this.webView.ref.goBack();
+      return true;
+    }
+    return false;
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+    }
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'android') {
+      BackHandler.removeEventListener('hardwareBackPress');
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <WebView
+          ref={(webView) => { this.webView.ref = webView; }}
+          onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+          automaticallyAdjustContentInsets={false}
+          source={{uri: 'https://hashnode.com/community'}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          style={{marginTop: 25}}
+        />
     </View>
-  );
-}
+    )
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
