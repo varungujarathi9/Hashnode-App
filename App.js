4ecc686f96c42b699c8a79b92bcb05e1621edc2a
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-08-15 23:21:46 +0530

    working app

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
-import React, {Component} from 'react';
-import {View,BackHandler,Platform,} from 'react-native';
-import {WebView} from 'react-native-webview';
+import React, { Component } from 'react';
+import { View, BackHandler, Platform } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { WebView } from 'react-native-webview';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const Stack = createNativeStackNavigator();
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: '',
+              headerStyle: {
+                backgroundColor: '#3678FA',
+              },
+              headerTitleStyle: {
+                color: "#f4e530"
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
+
+}
+
+export class Home extends Component {
 
   constructor(props) {
     super(props);
@@ -21,7 +55,7 @@ export default class App extends Component {
     return false;
   }
 
-  componentWillMount() {
+  componentDidMount() {
     if (Platform.OS === 'android') {
       BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
     }
@@ -32,21 +66,27 @@ export default class App extends Component {
       BackHandler.removeEventListener('hardwareBackPress');
     }
   }
+  static navigationOptions = {
+    title: 'Demo',
+    headerStyle: {
+      height: 10
+    }
+  };
 
-  render() {
-    return (
-      <View style={{flex:1}}>
-        <WebView
-          ref={(webView) => { this.webView.ref = webView; }}
-          onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
-          automaticallyAdjustContentInsets={false}
-          source={{uri: 'https://hashnode.com/community'}}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          startInLoadingState={true}
-          style={{marginTop: 25}}
-        />
-    </View>
+  render(){
+    return(
+        <View style={{flex:1}}>
+          <WebView
+              ref={(webView) => { this.webView.ref = webView; }}
+              onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
+              automaticallyAdjustContentInsets={false}
+              source={{uri: 'https://hashnode.com/community'}}
+              javaScriptEnabled={true}
+              domStorageEnabled={true}
+              startInLoadingState={true}
+              style={{marginTop: 0}}
+            />
+        </View>
     )
   }
 
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 An iOS &amp; Android app built with React Native framework for Hashnode Community.  
 Visit HashNode - <https://hashnode.com/community>
+
+## Technical Details
+
+- The app currently serves the WebView of Hashnode Community website. Although the logo placement and header layout is distorted, it is a functionally working app.
+
+**Android .apk download URL**: [Click here](https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40varungujarathi9/hashnode-f6dcf0007e9c41f0b4a6dd9d8c6d3ca4-signed.apk)
